928bb987f917eb557f586edba81a9d1e74df6500
nutanek <nutcs@hotmail.com>
2017-12-07 16:22:33 +0700

    add function for random color

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-    
+    var colorCode = randomColor();
+    $('#color').text(colorCode);
+
+    function randomColor() {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+          color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
 });
+
